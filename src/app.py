2aca7f0e7cad3afb09c6f7f47ea69562a25010d3
Madhusudhan Konda <m.konda@chocolateminds.com>
2024-03-23 11:04:47 +0000

    updated with func

--- src/app.py
+++ src/app.py
@@ -3,13 +3,22 @@ import config, model
 from openai import OpenAI
 
 temperature=config.TEMPERATURE
+SYSTEM_MESSAGE=config.SYSTEM_MESSAGE
 
 llm = model.get_llm()
 
-res = llm.chat.completions.create(
-    model="gpt-4",
-    messages=[{"role":"system", "content":"You are a Tibetian Monk. Always answer in cryptic sense"},
-              {"role":"user", "content":"What is the meaning life?"}]
-)
+def ask_llm(query):
+    print(SYSTEM_MESSAGE)
+    print(query)
+    completion = llm.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role":"system", "content":SYSTEM_MESSAGE},
+            {"role":"user", "content":query}
+        ])
+    
+    return completion
+
+res = ask_llm("What is the meaning life?")
 
 print(res.choices[0].message.content)
